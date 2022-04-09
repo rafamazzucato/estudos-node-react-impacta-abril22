7e6b9d95d38a756dc1986efc084812e2692d8106
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2022-04-09 14:48:35 -0300

    ajuste

--- exercicio05/src/components/Component01.js
+++ exercicio05/src/components/Component01.js
@@ -1,10 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Component02 } from './Component02';
 
-export class Component01 extends Component{
+export class Component01 extends Component {
 
-    render(){
-        return(
-            <h1>Meu Primeiro Componente</h1>
+    render() {
+        return (
+            <>
+                <h1>Meu Primeiro Componente</h1>
+                <Component02 />
+            </>
         );
     }
 }
--- exercicio05/src/components/Container.js
+++ exercicio05/src/components/Container.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Component01 } from './Component01';
-import { Component02 } from './Component02';
 
 export const Container = () => (
     <div>
       <Component01 />
-      <Component02 />
     </div>
 );
